26789ce9ae4f53138fd4246a146ade91740729ae
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2023-01-07 08:59:26 -0600

    added and configured image_magick

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,2 +1,14 @@
 class Motorcycle < ApplicationRecord
+
+  validates :model, presence: true,
+  validates :description, presence: true,
+  validates :total_amount, presence: true,
+
+  has_one_attached :image
+  has_many_attached :pictures
+
+  def image_as_thumbnail
+    image.variant(resize_to_limit: [50, 50]).processed
+  end
+
 end
