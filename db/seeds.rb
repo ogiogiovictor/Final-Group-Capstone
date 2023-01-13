ccda28a2028c5559d086f03475ca8a0a87edc17a
Omachi John <omachi.john@yahoo.com>
2023-01-13 23:26:50 +0100

    generate seed files for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,27 @@
-User.create!(
-  username: 'christiano',
+user1 = User.create!(
+  username: 'John',
   password: 'password',
-  email: 'chris@home.com',
-  name: 'christian'
+  email: 'John@home.com',
+  name: 'John'
+)
+
+user2 = User.create!(
+  username: 'Victor',
+  password: 'password',
+  email: 'Victor@home.com',
+  name: 'Victor'
+)
+
+user3 = User.create!(
+  username: 'Fernando',
+  password: 'password',
+  email: 'Fernando@home.com',
+  name: 'Fernando'
 )
 
 
-4.times do |i|
   Car.create!(
-    user_id: 1,
+    user_id: user1.id,
     brand: "Mercedes #{1}",
     model: 'benz',
     release_year: 2009,
@@ -18,10 +31,39 @@ User.create!(
     wheel_drive: 'four-wheel',
     price: 30000
   )
-end
 
-4.times do |i|
-  Reservation.create!(
-    date: Date.new, reserved_from: Date.new, reserved_until: Date.new, user: User.first, car: Car.first
+  Car.create!(
+    user_id: user2.id,
+    brand: "Mercedes #{1}",
+    model: 'benz',
+    release_year: 2009,
+    color: 'black',
+    transmission: 'manual',
+    seats: 6,
+    wheel_drive: 'four-wheel',
+    price: 30000
   )
-end
+
+  Car.create!(
+    user_id: user3.id,
+    brand: "Mercedes #{1}",
+    model: 'benz',
+    release_year: 2009,
+    color: 'black',
+    transmission: 'manual',
+    seats: 6,
+    wheel_drive: 'four-wheel',
+    price: 30000
+  )
+
+  Reservation.create!(
+    date: Date.new, reserved_from: Date.new, reserved_until: Date.new, user: user1, car: Car.first
+  )
+
+  Reservation.create!(
+    date: Date.new, reserved_from: Date.new, reserved_until: Date.new, user: user2, car: Car.second
+  )
+
+  Reservation.create!(
+    date: Date.new, reserved_from: Date.new, reserved_until: Date.new, user: user3, car: Car.third
+  )
