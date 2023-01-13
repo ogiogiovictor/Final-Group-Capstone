ac45334c31e60f278f61ed9dc8b77fede5b019d8
Omachi John <omachi.john@yahoo.com>
2023-01-13 23:16:52 +0100

    Bootstraps app to rails api model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
-# class ApplicationController < ActionController::API
-#   before_action :configure_permitted_parameters, if: :devise_controller?
+class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-#   protected
+  protected
 
-#   def configure_permitted_parameters
-#     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username password])
-#   end
-# end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username password])
+  end
+end
