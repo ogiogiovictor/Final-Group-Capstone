89323961c16c1ec68bb7edb9272c1d9f4792dc5f
Fernando Silva <fernando.silvabr66@gmail.com>
2023-01-16 20:22:07 -0300

    Update routes files with API\V1 structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :bookings
-  resources :motorcycles
+
+  namespace :api do
+    namespace :v1 do
+      get 'motorcycles', to: 'motorcycles#all_motorcycles'
+      resources :users, except: [:new] do
+        resources :motorcycles
+        resources :bookings
+      end
+    end
+  end
+
+  # resources :users
+  # resources :bookings
+  # resources :motorcycles
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
