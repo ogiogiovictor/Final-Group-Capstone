9433a48e770ced8a3ad62bb80b9403f2a7c16d52
Omachi John <omachi.john@yahoo.com>
2023-01-25 23:03:54 +0100

    commit readme file

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 class Car < ApplicationRecord
-    belongs_to :user
-    paginates_per 3
-    validates :brand, :model, :release_year, :color, :transmission, :seats, :wheel_drive, :price, presence: true
-  end
-  
+  belongs_to :user
+  paginates_per 3
+  validates :brand, :model, :release_year, :color, :transmission, :seats, :wheel_drive, :price, presence: true
+end
