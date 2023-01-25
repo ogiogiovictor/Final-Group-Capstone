cd928cd7fc1b225afbdf51df9c364c1c9acbedc7
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2023-01-25 14:59:25 -0600

    added loop

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,17 @@ User.create!(
     email: 'John@home.com',
     name: 'John'
   )
+
+  4.times do |i|
+    Car.create!(
+      user_id: 1,
+      brand: "Camery #{i + 1}",
+      model: 'Toyota',
+      release_year: 2009,
+      color: 'white',
+      transmission: 'manual',
+      seats: 6,
+      wheel_drive: 'four-wheel',
+      price: 30000
+    )
+  end
